b96777e7354165fb47b3bba325af33f4e1c16b43
sh1nh0 <dash.emerald80@gmail.com>
2024-11-15 14:45:21 +0900

    Add files via upload

--- js/script4.js
+++ js/script4.js
@@ -1,16 +1,21 @@
-$(function(){
-  $(".menu").mouseenter(function(){
-      $(this).css({
-        "background-color: #74b754;"
-        "border-radius: 40px;"
-        "color: #fff;"
-        "width: 130px;"
+// $(document).ready(function(){
+//   $(".menu li a").on("mouseover", function(){
+//     $(this).css(
+//       "background-color", "#74b754");
+//   $(this).css(
+//       "color", "#fff");
+//       $(this).css(
+//             "border","#74b754 1px solid;");
+//   });
 
+//   $(".menu li a").on("mouseleave", function(){
+//     $(this).css(
+//       "background-color", "#fff");
+//   $(this).css(
+//       "color", "inherit");
+//       $(this).css(
+//             "border","#ddd 1px solid;");
+//   });
 
-      });
-
-
-  });
-
-
-});
+  
+// });
