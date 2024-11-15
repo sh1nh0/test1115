16c508a5233c131a3300223c2bea458832a68366
sh1nh0 <dash.emerald80@gmail.com>
2024-11-15 10:51:37 +0900

    Add files via upload

---
+++ js/script4.js
@@ -0,0 +1,16 @@
+$(function(){
+  $(".menu").mouseenter(function(){
+      $(this).css({
+        "background-color: #74b754;"
+        "border-radius: 40px;"
+        "color: #fff;"
+        "width: 130px;"
+
+
+      });
+
+
+  });
+
+
+});
